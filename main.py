8fbba44c354d7bca8c17f51cbb23aeb94d0c1195
axel-flores <axelpsb@gmail.com>
2025-07-15 20:42:32 -0600

    cli AI agent using python and Gemini API

---
+++ main.py
@@ -0,0 +1,44 @@
+import argparse
+import sys
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+def main():
+    load_dotenv()  # Load environment variables from .env file
+    print("Hello from paigent!")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user_prompt', help='prompt o instrucciones a realizar')
+    parser.add_argument('--verbose', action='store_true')
+    args = parser.parse_args()
+
+    gemini_api_key = os.getenv("GEMINI_API_KEY")
+    if not gemini_api_key:
+        print("Error: api key not found in .env file")
+        sys.exit(1)
+
+    try:
+        client = genai.Client(api_key=gemini_api_key)
+
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=args.user_prompt)]),
+        ]
+
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', contents=messages
+        )
+
+        if args.verbose:
+            print(f"User prompt:{args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+        print(response.text)
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
